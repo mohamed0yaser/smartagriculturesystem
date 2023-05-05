2ba5eb41528821367f44203e5db891a69a6c97ba
mohamed0yaser <mohamedyaser2023@gmail.com>
2023-05-05 17:16:05 +0200

    deployment

--- smartagrisystem/settings.py
+++ smartagrisystem/settings.py
@@ -129,4 +129,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-#CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ALL_ORIGINS = "https://smartagricalituresystem.azurewebsites.net"
