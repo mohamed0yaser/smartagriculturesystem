1cc7b146dae3fffe3331962edbc24a6998f4041e
mohamed0yaser <90569015+mohamed0yaser@users.noreply.github.com>
2023-05-11 10:38:23 +0300

    -----

--- sas/settings.py
+++ sas/settings.py
@@ -28,7 +28,15 @@ SECRET_KEY = 'django-insecure-p+lm(b$+le@rsq&bwtn$3js=-$pyccs@e7%6qmr0khzcwcv+jw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    '[::1]',
+    'smartagricalituresystem.azurewebsites.net', 
+]
+
+
 
 
 # Application definition
